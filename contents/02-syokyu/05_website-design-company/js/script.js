115866fba9c72ca32627e28fef47054727d45c45
YasushiSueyoshi <53155701+YasushiSueyoshi@users.noreply.github.com>
2021-10-10 12:01:07 +0900

    Add files via upload

---
+++ contents/02-syokyu/05_website-design-company/js/script.js
@@ -0,0 +1,14 @@
+// #page-topをクリックした際の設定
+$('#page-top').click(function () {
+    $('body,html').animate({
+        scrollTop: 0//ページトップまでスクロール
+    }, 500);//ページトップスクロールの速さ。数字が大きいほど遅くなる
+    return false;//リンク自体の無効化
+});
+
+$('.head_nav_item a[href*="#"]').click(function () {
+    var elmHash = $(this).attr('href'); //ページ内リンクのHTMLタグhrefから、リンクされているエリアidの値を取得
+    var pos = $(elmHash).offset().top;  //idの上部の距離を取得
+    $('body,html').animate({ scrollTop: pos }, 500); //取得した位置にスクロール。500の数値が大きくなるほどゆっくりスクロール
+    return false;
+});
