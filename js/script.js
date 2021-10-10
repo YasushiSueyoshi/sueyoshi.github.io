77250fa31575ada8bb6d6f912967607670dfe531
YasushiSueyoshi <53155701+YasushiSueyoshi@users.noreply.github.com>
2021-10-10 11:27:03 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,72 @@
+//スクロールした際の動きを関数でまとめる
+function PageTopAnime() {
+
+  var scroll = $(window).scrollTop(); //スクロール値を取得
+  if (scroll >= 200) {//200pxスクロールしたら
+    $('#page-top').removeClass('DownMove');   // DownMoveというクラス名を除去して
+    $('#page-top').addClass('UpMove');      // UpMoveというクラス名を追加して出現
+  } else {//それ以外は
+    if ($('#page-top').hasClass('UpMove')) {//UpMoveというクラス名が既に付与されていたら
+      $('#page-top').removeClass('UpMove'); //  UpMoveというクラス名を除去し
+      $('#page-top').addClass('DownMove');  // DownMoveというクラス名を追加して非表示
+    }
+  }
+
+  var wH = window.innerHeight; //画面の高さを取得
+  var footerPos = $('#footer').offset().top; //footerの位置を取得
+  if (scroll + wH >= (footerPos + 10)) {
+    var pos = (scroll + wH) - footerPos + 10 //スクロールの値＋画面の高さからfooterの位置＋10pxを引いた場所を取得し
+    $('#page-top').css('bottom', pos); //#page-topに上記の値をCSSのbottomに直接指定してフッター手前で止まるようにする
+  } else {//それ以外は
+    if ($('#page-top').hasClass('UpMove')) {//UpMoveというクラス名がついていたら
+      $('#page-top').css('bottom', '10px');// 下から10pxの位置にページリンクを指定
+    }
+  }
+}
+
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+  PageTopAnime();/* スクロールした際の動きの関数を呼ぶ*/
+});
+
+// ページが読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load', function () {
+  PageTopAnime();/* スクロールした際の動きの関数を呼ぶ*/
+});
+
+// #page-topをクリックした際の設定
+$('#page-top').click(function () {
+  $('body,html').animate({
+    scrollTop: 0//ページトップまでスクロール
+  }, 500);//ページトップスクロールの速さ。数字が大きいほど遅くなる
+  return false;//リンク自体の無効化
+});
+
+
+//テキストのカウントアップの設定
+var bar = new ProgressBar.Line(splash_text, {//id名を指定
+  strokeWidth: 0,//進捗ゲージの太さ
+  duration: 1000,//時間指定(1000＝1秒)
+  trailWidth: 0,//線の太さ
+  text: {//テキストの形状を直接指定
+    style: {//天地中央に配置
+      position: 'absolute',
+      left: '50%',
+      top: '50%',
+      padding: '0',
+      margin: '0',
+      transform: 'translate(-50%,-50%)',
+      'font-size': '2.5rem',
+      color: '#2C7AA1',
+    },
+    autoStyleContainer: false //自動付与のスタイルを切る
+  },
+  step: function (state, bar) {
+    bar.setText(Math.round(bar.value() * 100) + ' %'); //テキストの数値
+  }
+});
+
+//アニメーションスタート
+bar.animate(1.0, function () {//バーを描画する割合を指定します 1.0 なら100%まで描画します
+  $("#splash").delay(500).fadeOut(800);//アニメーションが終わったら#splashエリアをフェードアウト
+});
